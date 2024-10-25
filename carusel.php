b68a1824b0e9bf833f283399647516bf0afdc985
Th0rSOnM0R4 <148124577+ThORsonMora@users.noreply.github.com>
2024-10-25 14:07:52 +0200

    templetes

---
+++ carusel.php
@@ -0,0 +1,37 @@
+  <!-- slideshow container -->
+  <section class= "slide-container">
+            <!-- full widdth images with numbers and text captions -->
+            <div class="mySlides fade">
+                <div class="numbertext">1/4</div>
+                <img src="./img/leknesokker2.png" alt="web leknesokker.no" width="100%">
+                <div class="text">CaptionText</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2/4</div>
+                <img src="./img/skatrgaver.png" alt="web skattgaver.no" width="100%">
+                <div class="text">CaptionText</div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3/4</div>
+                <img src="./img/soveland.png" alt="web soveland.no" width="100%">
+                <div class="text">CaptionText</div>
+            </div>
+            <div class="mySlides fade">
+                <div class="numbertext">4/4</div>
+                <img src="./img/leknesokker.png" alt="logo leknesoer.no" width="100%">
+                <div class="text">CaptionText</div>
+            </div>
+                <!-- next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </section>
+
+        <!-- the dots/circles -->
+         <section class="dot-bar">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+            <span class="dot" onclick="currentSlide(4)"></span>
+         </section>
